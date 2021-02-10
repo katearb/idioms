e7d2882ef19caa2ea8c514564dc8c1b73c291bed
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2021-02-10 17:14:29 +0300

    get number of abbrs in all dicts

--- combining_dicts.py
+++ combining_dicts.py
@@ -23,6 +23,8 @@ current_dict_left = set()
 for phrase in key_dict:
     all_phrases.union(set([leave_letters_only(p) for p in phrase['phrase'] if len(leave_letters_only(p)) > 1]))
 
+num_abbrs = 0
+
 for dictionary in [fedosov, kveselevich, myurrey, volkova]:
     for article in dictionary:
 
@@ -31,16 +33,18 @@ for dictionary in [fedosov, kveselevich, myurrey, volkova]:
                 for key_phrase in key_dict:
                     if leave_letters_only(phrase) in [leave_letters_only(p) for p in key_phrase['phrase']]:
                         key_phrase['semantics'].extend(article['semantics'])
+                        num_abbrs += sum([len(dic.get('abbr', [])) for dic in article['semantics']])
                         break
 
                 break
         else:
             current_dict_left.union(set([leave_letters_only(phr) for phr in article['phrase']]))
             key_dict.append(article)
+            num_abbrs += sum([len(dic.get('abbr', [])) for dic in article['semantics']])
 
     all_phrases.union(current_dict_left)
 
-
+print(num_abbrs)
 with open('all_idioms.json', 'w', encoding='utf8') as fp:
     json.dump(key_dict, fp, ensure_ascii=False, indent=4)
 
