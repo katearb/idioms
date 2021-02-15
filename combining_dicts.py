71114b592941cecbee4012177ab79ecd65dd4fce
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2021-02-15 23:54:12 +0300

    count both abbrs and phrases

--- combining_dicts.py
+++ combining_dicts.py
@@ -16,10 +16,12 @@ with open('fedorov.json', 'r', encoding='utf-8') as file:
 def leave_letters_only(phrase):
     return re.sub(r'[^а-я]', '', phrase.lower())
 
-num_abbrs = 0
+
 dict_abbrs = {}
+style_phrase = {}
 
 key_dict = fedorov
+style_phrase[key_dict[0]['semantics'][0]['dictionary']] = {'phrases': len(key_dict)}
 all_phrases = set()
 current_dict_left = set()
 for phrase in key_dict:
@@ -27,18 +29,17 @@ for phrase in key_dict:
     abbrs_here = sum([len(dic.get('abbr', [])) for dic in phrase['semantics']])
     dict_abbrs[phrase['semantics'][0]['dictionary']] = dict_abbrs.get(
         phrase['semantics'][0]['dictionary'], 0) + abbrs_here
-    num_abbrs += abbrs_here
 
 for dictionary in [fedosov, kveselevich, myurrey, volkova]:
-    for article in dictionary:
+    style_phrase[dictionary[0]['semantics'][0]['dictionary']] = {'phrases': len(dictionary)}
 
+    for article in dictionary:
         for phrase in article['phrase']:
             if leave_letters_only(phrase) in all_phrases:
                 for key_phrase in key_dict:
                     if leave_letters_only(phrase) in [leave_letters_only(p) for p in key_phrase['phrase']]:
                         key_phrase['semantics'].extend(article['semantics'])
                         abbrs_here = sum([len(dic.get('abbr', [])) for dic in article['semantics']])
-                        num_abbrs += abbrs_here
                         dict_abbrs[article['semantics'][0]['dictionary']] = dict_abbrs.get(
                             article['semantics'][0]['dictionary'], 0) + abbrs_here
                         break
@@ -47,15 +48,19 @@ for dictionary in [fedosov, kveselevich, myurrey, volkova]:
         else:
             current_dict_left.union(set([leave_letters_only(phr) for phr in article['phrase']]))
             key_dict.append(article)
-            num_abbrs += sum([len(dic.get('abbr', [])) for dic in article['semantics']])
+            abbrs_here = sum([len(dic.get('abbr', [])) for dic in article['semantics']])
             dict_abbrs[article['semantics'][0]['dictionary']] = dict_abbrs.get(
-                article['semantics'][0]['dictionary'], 0
-            ) + len(article['semantics'][0].get('abbr', []))
+                article['semantics'][0]['dictionary'], 0) + abbrs_here
 
     all_phrases.union(current_dict_left)
 
-print(num_abbrs)
 print(dict_abbrs)
-with open('all_idioms.json', 'w', encoding='utf8') as fp:
-    json.dump(key_dict, fp, ensure_ascii=False, indent=4)
+print(sum(dict_abbrs.values()))
+
+for dic in dict_abbrs:
+    style_phrase[dic]['abbrs'] = dict_abbrs[dic]
+    print(dic, style_phrase[dic])
+
+# with open('all_idioms.json', 'w', encoding='utf8') as fp:
+    # json.dump(key_dict, fp, ensure_ascii=False, indent=4)
 
